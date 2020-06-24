859eb0cfa9309b78ad7c732c1b1c5fc039c0d423
Ballantyne, Bob <bob.ballantyne@emc.com>
2020-06-24 18:40:46 -0400

    first commit

--- kafka/show-kafka-cluster.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-echo KAFKA_HOME =^> %KAFKA_HOME%
-echo KAFKA_CLUSTER =^> %KAFKA_CLUSTER%
-echo ZOOKEEPER_HOSTS =^> %ZOOKEEPER_HOSTS%
-echo KAFKA_BOOTSTRAP_SERVERS =^> %KAFKA_BOOTSTRAP_SERVERS%
-echo KAFKA_AVRO_HOSTS =^> %KAFKA_AVRO_HOSTS%
-echo.
-echo "To change cluster, run the command: set-kafka-cluster dev|dur|hop|local"
-echo.
